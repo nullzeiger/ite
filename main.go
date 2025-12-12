f1aa4e9a864e701a35ae053537613f80522904c2
nullzeiger <ivan.guerreschi.dev@gmail.com>
2025-12-12 20:04:32 +0100

    Add toolbar layout

--- main.go
+++ main.go
@@ -43,3 +43,14 @@ func (i *Ite) makeToolbar() {
 	i.pasteToolButton = i.toolbarFrame.TButton(Txt("Paste"))
 	i.exitToolButton = i.toolbarFrame.TButton(Txt("Exit"), Command(func() { Destroy(App) }))
 }
+
+func (i *Ite) layoutToolbar() {
+	opts := Opts{Sticky(W)}
+	Grid(i.newToolButton, Row(0), Column(1), opts)
+	Grid(i.openToolButton, Row(0), Column(2), opts)
+	Grid(i.saveToolButton, Row(0), Column(3), opts)
+	Grid(i.cutToolButton, Row(0), Column(4), opts)
+	Grid(i.copyToolButton, Row(0), Column(5), opts)
+	Grid(i.pasteToolButton, Row(0), Column(6), opts)
+	Grid(i.exitToolButton, Row(0), Column(7), opts)
+}
